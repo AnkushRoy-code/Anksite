6451d173c0a3dfd9ad6b253f61019e7a12350899
Ankush Roy <ankush3411111@gmail.com>
2024-06-26 21:43:25 +0530

    Initial commit + a beautiful background

---
+++ .svelte-kit/ambient.d.ts
@@ -0,0 +1,257 @@
+
+// this file is generated — do not edit it
+
+
+/// <reference types="@sveltejs/kit" />
+
+/**
+ * Environment variables [loaded by Vite](https://vitejs.dev/guide/env-and-mode.html#env-files) from `.env` files and `process.env`. Like [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), this module cannot be imported into client-side code. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
+ * 
+ * _Unlike_ [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), the values exported from this module are statically injected into your bundle at build time, enabling optimisations like dead code elimination.
+ * 
+ * ```ts
+ * import { API_KEY } from '$env/static/private';
+ * ```
+ * 
+ * Note that all environment variables referenced in your code should be declared (for example in an `.env` file), even if they don't have a value until the app is deployed:
+ * 
+ * ```
+ * MY_FEATURE_FLAG=""
+ * ```
+ * 
+ * You can override `.env` values from the command line like so:
+ * 
+ * ```bash
+ * MY_FEATURE_FLAG="enabled" npm run dev
+ * ```
+ */
+declare module '$env/static/private' {
+	export const SHELL: string;
+	export const npm_command: string;
+	export const USER_ZDOTDIR: string;
+	export const ZLE_RPROMPT_INDENT: string;
+	export const npm_config_userconfig: string;
+	export const COLORTERM: string;
+	export const npm_config_cache: string;
+	export const XDG_SESSION_PATH: string;
+	export const TERM_PROGRAM_VERSION: string;
+	export const POSH_PID: string;
+	export const I3SOCK: string;
+	export const NODE: string;
+	export const COLOR: string;
+	export const npm_config_local_prefix: string;
+	export const DESKTOP_SESSION: string;
+	export const NO_AT_BRIDGE: string;
+	export const npm_config_globalconfig: string;
+	export const EDITOR: string;
+	export const FZF_ALT_C_OPTS: string;
+	export const GTK_MODULES: string;
+	export const XDG_SEAT: string;
+	export const PWD: string;
+	export const LOGNAME: string;
+	export const XDG_SESSION_DESKTOP: string;
+	export const XDG_SESSION_TYPE: string;
+	export const npm_config_init_module: string;
+	export const _: string;
+	export const XAUTHORITY: string;
+	export const FZF_DEFAULT_COMMAND: string;
+	export const VSCODE_GIT_ASKPASS_NODE: string;
+	export const MOTD_SHOWN: string;
+	export const VSCODE_INJECTION: string;
+	export const HOME: string;
+	export const LANG: string;
+	export const XDG_CURRENT_DESKTOP: string;
+	export const POSH_SHELL_VERSION: string;
+	export const npm_package_version: string;
+	export const CONDA_PROMPT_MODIFIER: string;
+	export const GIT_ASKPASS: string;
+	export const XDG_SEAT_PATH: string;
+	export const INIT_CWD: string;
+	export const CHROME_DESKTOP: string;
+	export const npm_lifecycle_script: string;
+	export const VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
+	export const npm_config_npm_version: string;
+	export const XDG_SESSION_CLASS: string;
+	export const TERM: string;
+	export const POSH_PROMPT_COUNT: string;
+	export const npm_package_name: string;
+	export const FZF_CTRL_T_COMMAND: string;
+	export const POSH_CURSOR_COLUMN: string;
+	export const npm_config_prefix: string;
+	export const ZDOTDIR: string;
+	export const USER: string;
+	export const FZF_ALT_C_COMMAND: string;
+	export const VSCODE_GIT_IPC_HANDLE: string;
+	export const FZF_CTRL_T_OPTS: string;
+	export const PAM_KWALLET5_LOGIN: string;
+	export const DISPLAY: string;
+	export const npm_lifecycle_event: string;
+	export const SHLVL: string;
+	export const XDG_VTNR: string;
+	export const POSH_CURSOR_LINE: string;
+	export const XDG_SESSION_ID: string;
+	export const npm_config_user_agent: string;
+	export const npm_execpath: string;
+	export const POSH_THEME: string;
+	export const XDG_RUNTIME_DIR: string;
+	export const DEBUGINFOD_URLS: string;
+	export const npm_package_json: string;
+	export const VSCODE_GIT_ASKPASS_MAIN: string;
+	export const GTK3_MODULES: string;
+	export const XDG_DATA_DIRS: string;
+	export const GDK_BACKEND: string;
+	export const npm_config_noproxy: string;
+	export const PATH: string;
+	export const npm_config_node_gyp: string;
+	export const ORIGINAL_XDG_CURRENT_DESKTOP: string;
+	export const DBUS_SESSION_BUS_ADDRESS: string;
+	export const FZF_DEFAULT_OPTS: string;
+	export const npm_config_global_prefix: string;
+	export const MAIL: string;
+	export const POWERLINE_COMMAND: string;
+	export const npm_node_execpath: string;
+	export const npm_config_engine_strict: string;
+	export const OLDPWD: string;
+	export const TERM_PROGRAM: string;
+	export const NODE_ENV: string;
+}
+
+/**
+ * Similar to [`$env/static/private`](https://kit.svelte.dev/docs/modules#$env-static-private), except that it only includes environment variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Values are replaced statically at build time.
+ * 
+ * ```ts
+ * import { PUBLIC_BASE_URL } from '$env/static/public';
+ * ```
+ */
+declare module '$env/static/public' {
+	
+}
+
+/**
+ * This module provides access to runtime environment variables, as defined by the platform you're running on. For example if you're using [`adapter-node`](https://github.com/sveltejs/kit/tree/main/packages/adapter-node) (or running [`vite preview`](https://kit.svelte.dev/docs/cli)), this is equivalent to `process.env`. This module only includes variables that _do not_ begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) _and do_ start with [`config.kit.env.privatePrefix`](https://kit.svelte.dev/docs/configuration#env) (if configured).
+ * 
+ * This module cannot be imported into client-side code.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/private';
+ * console.log(env.DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ * 
+ * > In `dev`, `$env/dynamic` always includes environment variables from `.env`. In `prod`, this behavior will depend on your adapter.
+ */
+declare module '$env/dynamic/private' {
+	export const env: {
+		SHELL: string;
+		npm_command: string;
+		USER_ZDOTDIR: string;
+		ZLE_RPROMPT_INDENT: string;
+		npm_config_userconfig: string;
+		COLORTERM: string;
+		npm_config_cache: string;
+		XDG_SESSION_PATH: string;
+		TERM_PROGRAM_VERSION: string;
+		POSH_PID: string;
+		I3SOCK: string;
+		NODE: string;
+		COLOR: string;
+		npm_config_local_prefix: string;
+		DESKTOP_SESSION: string;
+		NO_AT_BRIDGE: string;
+		npm_config_globalconfig: string;
+		EDITOR: string;
+		FZF_ALT_C_OPTS: string;
+		GTK_MODULES: string;
+		XDG_SEAT: string;
+		PWD: string;
+		LOGNAME: string;
+		XDG_SESSION_DESKTOP: string;
+		XDG_SESSION_TYPE: string;
+		npm_config_init_module: string;
+		_: string;
+		XAUTHORITY: string;
+		FZF_DEFAULT_COMMAND: string;
+		VSCODE_GIT_ASKPASS_NODE: string;
+		MOTD_SHOWN: string;
+		VSCODE_INJECTION: string;
+		HOME: string;
+		LANG: string;
+		XDG_CURRENT_DESKTOP: string;
+		POSH_SHELL_VERSION: string;
+		npm_package_version: string;
+		CONDA_PROMPT_MODIFIER: string;
+		GIT_ASKPASS: string;
+		XDG_SEAT_PATH: string;
+		INIT_CWD: string;
+		CHROME_DESKTOP: string;
+		npm_lifecycle_script: string;
+		VSCODE_GIT_ASKPASS_EXTRA_ARGS: string;
+		npm_config_npm_version: string;
+		XDG_SESSION_CLASS: string;
+		TERM: string;
+		POSH_PROMPT_COUNT: string;
+		npm_package_name: string;
+		FZF_CTRL_T_COMMAND: string;
+		POSH_CURSOR_COLUMN: string;
+		npm_config_prefix: string;
+		ZDOTDIR: string;
+		USER: string;
+		FZF_ALT_C_COMMAND: string;
+		VSCODE_GIT_IPC_HANDLE: string;
+		FZF_CTRL_T_OPTS: string;
+		PAM_KWALLET5_LOGIN: string;
+		DISPLAY: string;
+		npm_lifecycle_event: string;
+		SHLVL: string;
+		XDG_VTNR: string;
+		POSH_CURSOR_LINE: string;
+		XDG_SESSION_ID: string;
+		npm_config_user_agent: string;
+		npm_execpath: string;
+		POSH_THEME: string;
+		XDG_RUNTIME_DIR: string;
+		DEBUGINFOD_URLS: string;
+		npm_package_json: string;
+		VSCODE_GIT_ASKPASS_MAIN: string;
+		GTK3_MODULES: string;
+		XDG_DATA_DIRS: string;
+		GDK_BACKEND: string;
+		npm_config_noproxy: string;
+		PATH: string;
+		npm_config_node_gyp: string;
+		ORIGINAL_XDG_CURRENT_DESKTOP: string;
+		DBUS_SESSION_BUS_ADDRESS: string;
+		FZF_DEFAULT_OPTS: string;
+		npm_config_global_prefix: string;
+		MAIL: string;
+		POWERLINE_COMMAND: string;
+		npm_node_execpath: string;
+		npm_config_engine_strict: string;
+		OLDPWD: string;
+		TERM_PROGRAM: string;
+		NODE_ENV: string;
+		[key: `PUBLIC_${string}`]: undefined;
+		[key: `${string}`]: string | undefined;
+	}
+}
+
+/**
+ * Similar to [`$env/dynamic/private`](https://kit.svelte.dev/docs/modules#$env-dynamic-private), but only includes variables that begin with [`config.kit.env.publicPrefix`](https://kit.svelte.dev/docs/configuration#env) (which defaults to `PUBLIC_`), and can therefore safely be exposed to client-side code.
+ * 
+ * Note that public dynamic environment variables must all be sent from the server to the client, causing larger network requests — when possible, use `$env/static/public` instead.
+ * 
+ * Dynamic environment variables cannot be used during prerendering.
+ * 
+ * ```ts
+ * import { env } from '$env/dynamic/public';
+ * console.log(env.PUBLIC_DEPLOYMENT_SPECIFIC_VARIABLE);
+ * ```
+ */
+declare module '$env/dynamic/public' {
+	export const env: {
+		[key: `PUBLIC_${string}`]: string | undefined;
+	}
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<main>
+	<div class="content">
+		<div class="list">
+			<div class="item"><img src="https://picsum.photos/200/300?random=1" alt="image" /></div>
+			<div class="item"><img src="https://picsum.photos/200/300?random=2" alt="image" /></div>
+			<div class="item"><img src="https://picsum.photos/200/300?random=3" alt="image" /></div>
+			<div class="item"><img src="https://picsum.photos/200/300?random=4" alt="image" /></div>
+			<div class="item"><img src="https://picsum.photos/200/300?random=5" alt="image" /></div>
+			<div class="item"><img src="https://picsum.photos/200/300?random=6" alt="image" /></div>
+			<div class="item"><img src="https://picsum.photos/200/300?random=7" alt="image" /></div>
+			<div class="item"><img src="https://picsum.photos/200/300?random=8" alt="image" /></div>
+			<div class="item"><img src="https://picsum.photos/200/300?random=9" alt="image" /></div>
+			<div class="item"><img src="https://picsum.photos/200/300?random=10" alt="image" /></div>
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		background: rgb(2, 2, 82);
+		background: linear-gradient(
+			139deg,
+			rgba(2, 2, 82, 1) 0%,
+			rgba(9, 9, 121, 1) 35%,
+			rgba(0, 212, 255, 1) 100%
+		);
+		height: 200vh;
+		width: 100vw;
+	}
+
+	.content {
+		height: 100vh;
+		width: 100vw;
+	}
+	.list {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 10px;
+
+		position: relative;
+		top: 50%;
+		transform: translateY(-50%);
+	}
+
+	.item {
+		flex: 0 0 auto;
+	}
+
+	.item img {
+		width: 9vw; /* Adjust the value to fit all images within the viewport width */
+		height: auto; /* Maintains aspect ratio */
+		display: block;
+	}
+</style>
