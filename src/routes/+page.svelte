2bd7e5969d796b353ddd5d9a5789fadd44041a41
Ankush Roy <ankush3411111@gmail.com>
2024-06-26 22:14:49 +0530

    added some fun stuff! check them out in the vercel site

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,6 @@
 			<div class="item"><img src="https://picsum.photos/200/300?random=5" alt="image" /></div>
 			<div class="item"><img src="https://picsum.photos/200/300?random=6" alt="image" /></div>
 			<div class="item"><img src="https://picsum.photos/200/300?random=7" alt="image" /></div>
-			<div class="item"><img src="https://picsum.photos/200/300?random=8" alt="image" /></div>
-			<div class="item"><img src="https://picsum.photos/200/300?random=9" alt="image" /></div>
-			<div class="item"><img src="https://picsum.photos/200/300?random=10" alt="image" /></div>
 		</div>
 	</div>
 </main>
@@ -32,24 +29,51 @@
 		height: 100vh;
 		width: 100vw;
 	}
+
+	.item img {
+		width: 10vw;
+		height: auto;
+		display: block;
+	}
+
 	.list {
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		gap: 10px;
+		gap: 1vw;
 
 		position: relative;
 		top: 50%;
-		transform: translateY(-50%);
+
+		transform-style: preserve-3d;
+		transform: perspective(1000px) translateY(-50%);
 	}
 
 	.item {
 		flex: 0 0 auto;
+		transition: 0.5s;
+		filter: brightness(0);
 	}
 
-	.item img {
-		width: 9vw; /* Adjust the value to fit all images within the viewport width */
-		height: auto; /* Maintains aspect ratio */
-		display: block;
+	.item:hover {
+		filter: brightness(1);
+		transform: translateZ(200px);
+	}
+
+	.item:hover + * {
+		filter: brightness(0.6);
+		transform: translateZ(150px) rotateY(30deg);
+	}
+	.item:hover + * + * {
+		filter: brightness(0.3);
+		transform: translateZ(80px) rotateY(20deg);
+	}
+	.item:has(+ *:hover) {
+		filter: brightness(0.6);
+		transform: translateZ(150px) rotateY(-30deg);
+	}
+	.item:has(+ * + *:hover) {
+		filter: brightness(0.3);
+		transform: translateZ(80px) rotateY(-20deg);
 	}
 </style>
